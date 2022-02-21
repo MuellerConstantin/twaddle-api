fbcc9449255ca58897c78c2258c2e531761bc913
Constantin Müller <32311121+0x1C1B@users.noreply.github.com>
2022-02-21 17:23:51 +0100

    docs: provide self-service documentation
    Write a minimal AsciiDoc based API documentation and provide it as
    self-service using static file serving.

--- src/app/index.js
+++ src/app/index.js
@@ -81,6 +81,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 app.use(cors());
 app.use(helmet());
 app.use(morgan());
